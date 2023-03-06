adc9f571ea5bc22a169f6e8ea227d844d992b334
joyrax-dev <joyrax.dev@gmail.com>
2023-03-06 21:10:59 +1000

    add hex functions to Color class. !!!HEX BETA!!!

--- Collary.UI/Graphics/Color.cs
+++ Collary.UI/Graphics/Color.cs
@@ -1,6 +1,7 @@
 ﻿using Collary.Native.SDL2;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,25 @@ public class Color
     public byte B { get; set; } = 0;
     public byte A { get; set; } = 0;
 
+    // !!!HEX BETA FUNCTIONS!!!
+    public string HEX 
+    { 
+        get => Color.RgbaToHex(this.R, this.G, this.B, this.A);
+        set {
+            byte[] rgba = Color.HexToRgba(value);
+            this.R = rgba[0];
+            this.G = rgba[1];
+            this.B = rgba[2];
+            this.A = rgba[3];
+        }
+    }
+
     public Color() { }
+
+    public Color(string hex) 
+    {
+        this.HEX = hex;
+    }
 
     public Color(byte r, byte g, byte b) : this(r, g, b, 255) { }
 
@@ -45,4 +64,42 @@ public class Color
         }
     }
     #endregion
+
+    public static byte[] HexToRgba(string hex)
+    {
+        if (hex.IndexOf('#') == -1)
+            throw new Exception("Parse error hex string, no indexed '#'");
+
+        hex = hex.Replace("#", "");
+        
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int a = 0;
+
+        if (hex.Length == 8)
+        {
+            //#RRGGBBAA
+            r = int.Parse(hex.Substring(0, 2), NumberStyles.HexNumber);
+            g = int.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
+            b = int.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
+            a = int.Parse(hex.Substring(6, 2), NumberStyles.HexNumber);
+        }
+
+        if (hex.Length == 6)
+        {
+            //#RRGGBB
+            r = int.Parse(hex.Substring(0, 2), NumberStyles.HexNumber);
+            g = int.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
+            b = int.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
+            a = 255;
+        }
+
+        return new byte[] {((byte)r), ((byte)g), ((byte)b), ((byte)a)};
+    }
+
+    public static string RgbaToHex(byte r, byte g, byte b, byte a)
+    {
+        return string.Format("#{0:X2}{1:X2}{2:X2}{3:X2}", r, g, b, a);
+    } 
 }
