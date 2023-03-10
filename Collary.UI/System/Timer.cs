9c3d77707d4651811c45b73d1917b2c3e106e262
joyrax-dev <joyrax.dev@gmail.com>
2023-03-10 21:57:47 +1000

    add timer

---
+++ Collary.UI/System/Timer.cs
@@ -0,0 +1,73 @@
+using Collary.Native.SDL2;
+using Collary.UI.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collary.UI.System;
+
+public class Timer
+{
+    protected ulong StartTicks { get; set;} = 0;
+    protected ulong PausedTicks { get; set; } = 0;
+
+    protected bool Started { get; set; } = false;
+    protected bool Paused { get; set; } = false;
+
+    public ulong Ticks 
+    {
+        get 
+        {
+            if (!this.Started)
+                return 0;
+
+            if (this.Paused)
+                return this.PausedTicks;
+            else
+                return SDL.SDL_GetTicks64() - this.StartTicks;
+        }
+    }
+
+    public void Start()
+    {
+        this.Started = true;
+        this.Paused = false;
+
+        this.PausedTicks = 0;
+        this.StartTicks = SDL.SDL_GetTicks64();
+    }
+
+    public void Stop()
+    {
+        this.Started = false;
+        this.Paused = false;
+
+        this.PausedTicks = 0;
+        this.StartTicks = 0;
+    }
+
+    public void Pause()
+    {
+        if (this.Started && !this.Paused)
+        {
+            this.Paused = true;
+
+            this.PausedTicks = SDL.SDL_GetTicks64() - this.StartTicks;
+            this.StartTicks = 0;
+        }
+    }
+
+    public void Unpause()
+    {
+        if (this.Started && this.Paused)
+        {
+            this.Paused = false;
+
+            this.StartTicks = SDL.SDL_GetTicks64() - this.PausedTicks;
+            this.PausedTicks = 0;
+        }
+    }
+}
